103b671ccf66daa15c5ea6f7d49655b10b98a443
Michał Startek <michal.startek@mimuw.edu.pl>
2022-03-07 13:49:48 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+from distutils.core import Extension
+import pathlib
+import opentimspy
+
+here = pathlib.Path(__file__).parent.resolve()
+
+setup(
+    name='envemind',
+    version='0.0.1',
+    description='Estimation of parameters in MIDIA experiments',
+#    long_description=(here / 'README.md').read_text(encoding='utf-8'),
+#    long_description_content_type='text/markdown',
+    url='https://github.com/PiotrRadzinski/envemind',
+    author='Piotr Radziński, Michał Piotr Startek',
+    classifiers=[
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3 :: Only',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            ],
+    keywords = 'Mass spectrimetry monisotopic peak',
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    python_requires='>=3.6',
+    install_requires='numpy scipy IsoSpec pyteomics'.split(),
+    entry_points={},
+    scripts=[''],
+)
